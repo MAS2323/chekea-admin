4aa5541413b7ecabcfe14350fcef4abb76fe804a
Ernesto Vicencio Mas <masoneweernesto@gmail.com>
2025-03-07 17:14:10 +0800

    actualizando

--- config/configApi.js
+++ config/configApi.js
@@ -1,5 +1,6 @@
-// export const API_URL =
-//   import.meta.env.VITE_API_URL ||
-//   "https://server-chekea-pedidos-production-a491.up.railway.app";
 export const API_URL =
-  import.meta.env.VITE_API_URL || "http://10.15.122.149:3000";
+  import.meta.env.VITE_API_URL ||
+  "https://server-chekea-pedidos-production-a491.up.railway.app";
+
+// export const API_URL =
+//   import.meta.env.VITE_API_URL || "http://10.15.122.149:3000";
